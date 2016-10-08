0451082cb63dadc6ac8a76b793b58d64e6a007a9
Alejandro Quisbert Carvajal <alexandro@autistici.org>
2016-10-08 19:59:16 -0400

    Conexión a base de datos Postgres

--- Saranani/Saranani/settings/local.py
+++ Saranani/Saranani/settings/local.py
@@ -11,8 +11,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'saranani',
+        'USER': 'alexandro',
+        'PASSWORD' : 'admin',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
